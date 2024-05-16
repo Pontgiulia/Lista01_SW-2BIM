393e363b0de1ed8b2ac5ecb07525d5d64dcae155
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-05-16 18:22:19 -0300

    Add files via upload

---
+++ Exercicio_07/Exercicio_07/Playlist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Playlist
+{
+    private List<Musica> musicas;
+    private string dono;
+
+    public Playlist(string dono)
+    {
+        musicas = new List<Musica>();
+        this.dono = dono;
+    }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void TocarMusicaAleatoria()
+    {
+        if (musicas.Count == 0)
+        {
+            Console.WriteLine("Playlist vazia. Adicione músicas antes de tocar.");
+            return;
+        }
+
+        Random random = new Random();
+        int indice = random.Next(0, musicas.Count);
+        Musica musicaTocada = musicas[indice];
+        Console.WriteLine($"Tocando a música: {musicaTocada.Nome}");
+    }
+}
---
+++ Exercicio_07/Exercicio_07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_07
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando algumas músicas
+            Musica musica1 = new Musica("A rua", "Jão", "Universal Music Group");
+            Musica musica2 = new Musica("Lábia", "Jão", "Universal Music Group");
+            Musica musica3 = new Musica("Me lambe", "Jão", "Universal Music Group");
+
+            // Criando uma playlist e adicionando as músicas
+            Playlist playlist = new Playlist("Usuário");
+            playlist.AdicionarMusica(musica1);
+            playlist.AdicionarMusica(musica2);
+            playlist.AdicionarMusica(musica3);
+
+            // Tocando uma música aleatória
+            playlist.TocarMusicaAleatoria();
+        }
+    }
+
+}
