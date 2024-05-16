76a1925ecf503469ac0329d08edad63453cfaeb8
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-05-16 18:10:28 -0300

    Add files via upload

---
+++ Exercicio_05/Exercicio_05/GerenciadorDeTarefas.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GerenciadorDeTarefas
+{
+    // Definindo a classe de Tarefa
+    public class Tarefa
+    {
+        public string Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public Tarefa(string descricao, DateTime dataVencimento)
+        {
+            Descricao = descricao;
+            DataVencimento = dataVencimento;
+        }
+
+        public override string ToString()
+        {
+            return $"Descrição: {Descricao}, Vencimento: {DataVencimento.ToShortDateString()}";
+        }
+
+        public bool DeveSerExecutadaHoje()
+        {
+            return DataVencimento.Date == DateTime.Today;
+        }
+    }
+
+    private List<Tarefa> tarefas;
+
+    // Construtor da classe GerenciadorDeTarefas
+    public GerenciadorDeTarefas()
+    {
+        tarefas = new List<Tarefa>();
+    }
+
+    // Método para adicionar uma tarefa à lista
+    public void AdicionarTarefa(string descricao, DateTime dataVencimento)
+    {
+        tarefas.Add(new Tarefa(descricao, dataVencimento));
+    }
+
+    // Método para remover uma tarefa da lista
+    public void RemoverTarefa(string descricao)
+    {
+        Tarefa tarefa = tarefas.FirstOrDefault(t => t.Descricao.Equals(descricao, StringComparison.OrdinalIgnoreCase));
+        if (tarefa != null)
+        {
+            tarefas.Remove(tarefa);
+            Console.WriteLine($"Tarefa \"{descricao}\" removida com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine($"Tarefa \"{descricao}\" não encontrada na lista.");
+        }
+    }
+
+    // Método para listar todas as tarefas
+    public void ListarTarefas()
+    {
+        Console.WriteLine("Lista de Tarefas:");
+        foreach (Tarefa tarefa in tarefas)
+        {
+            Console.WriteLine(tarefa);
+        }
+    }
+
+    // Método para verificar se há tarefas a serem executadas hoje
+    public void VerificarTarefasParaHoje()
+    {
+        List<Tarefa> tarefasParaHoje = tarefas.Where(t => t.DeveSerExecutadaHoje()).ToList();
+
+        if (tarefasParaHoje.Count > 0)
+        {
+            Console.WriteLine("Tarefas a serem executadas hoje:");
+            foreach (Tarefa tarefa in tarefasParaHoje)
+            {
+                Console.WriteLine(tarefa);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Não há tarefas a serem executadas hoje.");
+        }
+    }
+}
---
+++ Exercicio_05/Exercicio_05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
+
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Adicionar tarefa");
+            Console.WriteLine("2. Remover tarefa");
+            Console.WriteLine("3. Listar tarefas");
+            Console.WriteLine("4. Verificar tarefas para hoje");
+            Console.WriteLine("5. Sair");
+            Console.Write("Escolha uma opção: ");
+
+            int opcao;
+            if (int.TryParse(Console.ReadLine(), out opcao))
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Descrição da tarefa: ");
+                        string descricao = Console.ReadLine();
+                        Console.Write("Data de vencimento (DD/MM/AAAA): ");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime dataVencimento))
+                        {
+                            gerenciador.AdicionarTarefa(descricao, dataVencimento);
+                            Console.WriteLine("Tarefa adicionada com sucesso.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Data inválida.");
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Descrição da tarefa a ser removida: ");
+                        string descricaoRemover = Console.ReadLine();
+                        gerenciador.RemoverTarefa(descricaoRemover);
+                        break;
+                    case 3:
+                        gerenciador.ListarTarefas();
+                        break;
+                    case 4:
+                        gerenciador.VerificarTarefasParaHoje();
+                        break;
+                    case 5:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
