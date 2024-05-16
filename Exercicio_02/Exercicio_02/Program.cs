deec1f5bb9fab5d78e09d5298a1f92b0112f1785
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-05-16 16:58:13 -0300

    Add files via upload

---
+++ Exercicio_02/Exercicio_02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio_02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegistroDeCompras registro = new RegistroDeCompras();
+
+            // Adicionando algumas compras. meio obvio né, copia e cola e troca o produto e valor. o now do DT é pra aparecer a data do momento real mesmo
+            registro.AdicionarCompra(new Compra(DateTime.Now, "Notebook", 3500.00m));
+            registro.AdicionarCompra(new Compra(DateTime.Now, "Smartphone", 1500.00m));
+            registro.AdicionarCompra(new Compra(DateTime.Now, "Tablet", 850.00m));
+
+            // Listando as compras. não precisa mexer nisso aqui, só serve para mostrar a lista com a data, produto e preço
+            List<Compra> compras = registro.ListarCompras();
+            foreach (Compra compra in compras)
+            {
+                Console.WriteLine(compra);
+            }
+        }
+    }
+}
---
+++ Exercicio_02/Exercicio_02/RegistroDeCompras.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Compra
+{
+    //essa parte é pra definir que tipo de dado é cada coisa
+    public DateTime Data { get; set; }
+    public string Produto { get; set; }
+    public decimal Valor { get; set; }
+
+
+    //isso é o construtor, ele inicializa os atributos Data, Produto e Valor quando uma nova instância de Compra é criada.
+    public Compra(DateTime data, string produto, decimal valor)
+    {
+        //aqui é pra criar as variaveis e pra saber o que cada uma recebe
+        Data = data;
+        Produto = produto;
+        Valor = valor;
+    }
+
+    public override string ToString()
+    {
+        //o tostring é pra retornar uma representação em string da compra, formatando a data e o valor de uma maneira fácil de ler.
+        return $"Data: {Data.ToShortDateString()}, Produto: {Produto}, Valor: {Valor:C}";
+    }
+}
+
+public class RegistroDeCompras
+{
+    //aqui tá criando uma lista, então tudo que for adicionado em compras vem pra cá
+    private List<Compra> compras;
+
+
+    //esse é o construtor, ele inicializa a lista quando uma compra for criada
+    public RegistroDeCompras()
+    {
+        compras = new List<Compra>();
+    }
+
+    public void AdicionarCompra(Compra compra)
+    {
+        //isso é pra adicionar a compra
+        compras.Add(compra);
+    }
+
+    public List<Compra> ListarCompras()
+    {
+        //esse retorna a compra, aí ela aparece pra gente
+        return compras;
+    }
+}
