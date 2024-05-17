504e5a80e7a561213dc1633bfce8be80a9c50194
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-05-17 16:01:25 -0300

    Add files via upload

---
+++ Exercicio_12/Exercicio_12/Agenda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_12
+{
+
+    public class Agenda
+    {
+        private List<(string Nome, int Idade, float Altura)> pessoas;
+
+        // Construtor padrão para inicializar a lista de pessoas
+        public Agenda()
+        {
+            pessoas = new List<(string, int, float)>();
+        }
+
+        // Método para armazenar uma pessoa na agenda
+        public void ArmazenaPessoa(string nome, int idade, float altura)
+        {
+            pessoas.Add((nome, idade, altura));
+        }
+
+        // Método para remover uma pessoa da agenda pelo nome
+        public void RemovePessoa(string nome)
+        {
+            (string Nome, int Idade, float Altura) pessoa = BuscaPessoa(nome);
+            if (pessoa.Nome != null)
+            {
+                pessoas.Remove(pessoa);
+                Console.WriteLine($"Pessoa {nome} removida da agenda.");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa {nome} não encontrada na agenda.");
+            }
+        }
+
+        // Método para buscar uma pessoa na agenda pelo nome
+        public (string Nome, int Idade, float Altura) BuscaPessoa(string nome)
+        {
+            return pessoas.Find(p => p.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase));
+        }
+
+        // Método para imprimir os dados de todas as pessoas da agenda
+        public void ImprimeAgenda()
+        {
+            Console.WriteLine("Agenda:");
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Altura: {pessoa.Altura}m");
+            }
+        }
+    }
+
+}
---
+++ Exercicio_12/Exercicio_12/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_12
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Adicionar Pessoa");
+                Console.WriteLine("2. Remover Pessoa");
+                Console.WriteLine("3. Buscar Pessoa");
+                Console.WriteLine("4. Imprimir Agenda");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+
+                int opcao;
+                if (int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.Write("Nome da Pessoa: ");
+                            string nome = Console.ReadLine();
+                            Console.Write("Idade da Pessoa: ");
+                            int idade = int.Parse(Console.ReadLine());
+                            Console.Write("Altura da Pessoa (em metros): ");
+                            float altura = float.Parse(Console.ReadLine());
+                            agenda.ArmazenaPessoa(nome, idade, altura);
+                            Console.WriteLine("Pessoa adicionada com sucesso.");
+                            break;
+                        case 2:
+                            Console.Write("Nome da Pessoa a ser removida: ");
+                            string nomeRemover = Console.ReadLine();
+                            agenda.RemovePessoa(nomeRemover);
+                            break;
+                        case 3:
+                            Console.Write("Nome da Pessoa a ser buscada: ");
+                            string nomeBuscar = Console.ReadLine();
+                            (string Nome, int Idade, float Altura) pessoaEncontrada = agenda.BuscaPessoa(nomeBuscar);
+                            if (pessoaEncontrada.Nome != null)
+                            {
+                                Console.WriteLine($"Pessoa encontrada: Nome: {pessoaEncontrada.Nome}, Idade: {pessoaEncontrada.Idade}, Altura: {pessoaEncontrada.Altura}m");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Pessoa não encontrada na agenda.");
+                            }
+                            break;
+                        case 4:
+                            agenda.ImprimeAgenda();
+                            break;
+                        case 5:
+                            Console.WriteLine("Saindo...");
+                            return;
+                        default:
+                            Console.WriteLine("Opção inválida.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida.");
+                }
+            }
+        }
+    }
+
+}
